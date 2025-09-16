777237cfac8b189231dc907ba9db515c2b08e075
mvidal-ort <70246801+mvidal-ort@users.noreply.github.com>
2025-09-15 21:08:06 -0300

    10

--- Practico/0-Cero/DefensaAnterior.hs
+++ Practico/0-Cero/DefensaAnterior.hs
@@ -19,7 +19,7 @@ import Data.List (subsequences)
 data E = 
     Var X          -- variable
     | Assig X E      -- asignación
-    | MT Mat          -- matriz literal 2x2
+    | Matriz Mat     -- matriz literal 2x2
     | Scale Z E      -- escalado: k * M
     | Add E E        -- suma de matrices
     | Sub E E        -- resta de matrices
@@ -50,6 +50,23 @@ upd ((x,v):xs) (x0,v0)
     | (x == x0) = (x0,v0):xs
     | otherwise = (x,v):(upd xs (x0,v0))
 
+scaleMat :: Int -> Mat -> Mat
+scaleMat k (Mat a b c d) = Mat (k*a) (k*b) (k*c) (k*d)
+
+addMat :: Mat -> Mat -> Mat
+addMat (Mat a b c d) (Mat a' b' c' d') = Mat (a+a') (b+b') (c+c') (d+d')
+
+substMat :: Mat -> Mat -> Mat
+substMat (Mat a b c d) (Mat a' b' c' d') = Mat (a-a') (b-b') (c-c') (d-d')
+
+traceMat :: Mat -> Int
+traceMat (Mat a b c d) = a+b
+
+transposeMat :: Mat -> Mat
+transposeMat (Mat a b c d) = Mat a c b d
+
+isDiagonalMat :: Mat -> Bool
+isDiagonalMat (Mat a b c d) = b == 0 && c == 0
 ------------------------
 
 
@@ -61,7 +78,36 @@ eval m (Assig x e) =
     let (m', v) = eval m e
     in (upd m' (x,v), v)
 
-eval m (MT matriz) =
+eval m (Matriz matriz) = (m, VM matriz)
 
+-- eval m (Scale k e) = 
+--     let (m', v) = eval m e in case v of 
+--         VM mat -> (m', VM (scaleMat k mat)) 
+--         _ -> error "Scale aplicado a no-matriz"
+-- -- Segura porque verifca el tipo del valor obtenido en eval en tiempo de ejecucion
 
+eval m (Scale k e) =
+    let (m', VM v) = eval m e
+    in (m', VM (scaleMat k v))
 
+eval m (Add e1 e2) =
+    let (m', VM v1) = eval m e1
+        (m'', VM v2) = eval m' e2    
+    in (m'', VM (addMat v1 v2))
+
+eval m (Sub e1 e2) =
+    let (m', VM v1) = eval m e1
+        (m'', VM v2) = eval m' e2    
+    in (m'', VM (substMat v1 v2))
+
+eval m (Trace e) =
+    let (m', VM v) = eval m e
+    in (m', VI (traceMat v))
+
+eval m (Transpose e) =
+    let (m', VM v) = eval m e
+    in (m', VM (transposeMat v))
+
+eval m (IsDiagonal e) =
+    let (m', VM v) = eval m e
+    in (m', VB (isDiagonalMat v))
