d927d1e4cd8e048516135ddb3b96c2d7e6fa179c
mvidal-ort <ignacioserv@yahoo.com>
2025-10-15 15:57:38 -0300

    P2-01

--- Practico/2-Practico2/Practico2.hs
+++ Practico/2-Practico2/Practico2.hs
@@ -3,3 +3,27 @@
 {-# OPTIONS_GHC -fno-warn-missing-methods #-}
 
 module Practico2 where
+
+import Prelude
+
+data Program = 
+    Asig [(Id, E)] 
+    | Local [Id] Program 
+    | Sec Program Program 
+    | Case Id [B] 
+    | While Id [B] 
+    deriving (Show) 
+
+data E = 
+    K Id [E] 
+    | Var Id 
+    deriving (Show) 
+
+data V = 
+    Kv Id [V] 
+    | NULL 
+    deriving (Show) 
+
+type Id = String 
+type B = (Id, ([Id], Program)) 
+type M = [(Id, V)]
