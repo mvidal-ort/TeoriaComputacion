c03329e6d29e16834d5b3ee51cba14a4a2a5486b
mvidal-ort <ignacioserv@yahoo.com>
2025-10-17 16:04:53 -0300

    P2-02

--- Practico/2-Practico2/Practico2.hs
+++ Practico/2-Practico2/Practico2.hs
@@ -27,3 +27,41 @@ data V =
 type Id = String 
 type B = (Id, ([Id], Program)) 
 type M = [(Id, V)]
+
+lookupM :: Id -> M -> Maybe V
+lookupM _ [] = Nothing
+lookupM x' ((x,v):xs)
+    | x' == x = Just v
+    | otherwise = lookupM x xs
+
+updateM :: [(Id,V)] -> M -> M
+updateM [] m = m
+updateM ((x,v): xs) m = updateM xs (updateOne x v m)
+
+updateOne :: Id -> V -> M -> M
+updateOne x' v' [] = [(x',v')] 
+updateOne x' v' ((x,v):xs) 
+    | x' == x = (x', v'):xs
+    | otherwise = updateOne x' v' xs
+
+altaM :: [Id] -> M -> M
+altaM xs m = map (\x -> (x,NULL)) xs ++ m
+
+bajaM :: [Id] -> M -> M
+bajaM [] m = m
+bajaM (x:xs) m = bajaM xs (bajaOne x m)
+
+bajaOne :: Id -> M -> M
+bajaOne _ [] = []
+bajaOne x' ((x,v):xs)
+    | x' == x = xs
+    | otherwise = (x,v):bajaOne x' xs
+
+eval :: E -> M -> V
+eval (K c es) m = Kv c (map (`eval` m) es )
+eval (Var x) m = case lookupM x m of 
+                Just v -> v
+                Nothing -> error "no existe "
+ 
+
+
